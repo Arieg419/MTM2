2a1251e98ae180e7ee9724a53b48b1f0e43f7544
Omer Arie Goldberg <arieg419@gmail.com>
2014-11-27 16:45:04 -0500

    testUpdate

--- tester.c
+++ tester.c
@@ -3,240 +3,304 @@
 #include <stdlib.h>
 #include "conversation.h"
 #include <stdbool.h>
+#include "message.h"
+#include <string.h>
 
-#define TEST_RESULT_IS(flag, funcToRun(), result) if((funcToRun) != result) {
-	flag = false;
+#define CHECK_ARG_NULL(arg, returnValue) if(!arg) { \
+											return returnValue; \
+										}
+
+#define TEST_RESULT_IS(flag, funcToRun, result) if (flag && (funcToRun != result)) { \
+		flag = false; \
+	}
+
+#define ARG_IS_NULL(arg, returnValue) if(!arg){ \
+										return returnValue; \
+										}
+#define INITIALIZING_ARRAY(arg, n) 	for(int i =0; i<n; i++) { \
+														arg[i] = 0; \
+														}
+
+#define RETURN_CONV_RESULT(arg, correct, returnValue) if(arg == correct) { \
+														return returnValue; \
+													 }
+
+
+bool conversation_test_Combo();
+
+static unsigned char** allocateImage(unsigned height, unsigned width) {
+	unsigned char** array;
+	array = malloc(height*sizeof(unsigned char*));
+	CHECK_ARG_NULL(array, NULL)
+	for(int i = 0; i < height; i ++) {
+		array[i] = malloc(width*sizeof(unsigned char));
+		if(array[i] == NULL) {
+			for(int j=0; j<i; j++) {
+				free(array[j]);
+			}
+			free(array);
+			return NULL;
+		}
+	}
+	return array;
 }
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+/*
+static void copyMatrixValues(const unsigned char* const* src,
+		 unsigned char** dst, unsigned height, unsigned width) {
+	int i, j;
+	for (i = 0 ; i < height ; i++) {
+		for(j = 0; j < width ; j++) {
+			dst[i][j] = src[i][j];
+		}
+	}
+}
+*/
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+/*
+static bool dimensionCheckIsTrue (unsigned height, unsigned width) {
+	if ((height <= 0 || width <= 0) ) {
+		return false;
+	} else {
+		return true;
+	}
+}
+*/
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+static void freeOldImage(unsigned char** image, unsigned int height) {
+	 if(image == NULL) {
+		 return;
+	 }
+	for(int i = 0; i < height; i ++) {
+		free(image[i]);
+	}
+	free(image);
+}
+
+
+//!!!!!!!!!!!!!!!!!! MESSAGE IMAGE CREATE !!!!!!!!!!!!!!!!!
+/*
+Message messageImageCreate( int senderID, const unsigned char* const* image,
+		 unsigned width, unsigned height){
+	if(image == NULL) {
+			return NULL;
+		}
+	if (!(dimensionCheckIsTrue(height, width))) {
+		return NULL;
+	}
+	Message newImageMessage = malloc(sizeof(struct message_t));
+	CHECK_ARG_NULL(newImageMessage, NULL)
+	unsigned char** array = allocateImage(height, width);
+	if(array == NULL) {
+		free(newImageMessage);
+		return NULL;
+	}
+
+	copyMatrixValues(image, array, height, width);
+	newImageMessage->content.image.data = array;
+	newImageMessage->senderID = senderID;
+	newImageMessage->content.image.height = height;
+	newImageMessage->content.image.width = width;
+	newImageMessage->type = MESSAGE_IMAGE;
+	return newImageMessage;
+}
+*/
+//!!!!!!!!!!!!!!!!!! MESSAGE IMAGE CREATE !!!!!!!!!!!!!!!!!!!!!!!!
+
+
+
 
 int main() {
-	//Run test script here
-	return 0;
-}
-
-if(!flag) {
-	printf("uh oh\n");
-}
-//!!!!!!!!!!!!!!!!!!!!!! 
-// ALLOCATE NEw IMAGE FUNC HERE
-//!!!!!!!!!!!!!!!!!!!!!!!
-
-bool conversationTest() {
-
-ConversationResult convoResult;
-
-Conversation convo1 = conversationCreate(5);
-Conversation convo2  = conversationCopy(convo1);
-
-Message textMsg1 = messageTextCreate(302365697, "hahahaha");
-Message textMsg2 = messageTextCreate(132424, "this is it");
-Message textMsg3 = messageTextCreate(132424, "");
-Message textMsg4 = messageTextCreate(302365697, "YOLO");
-
-//Message imgMsg = messageImageCreate()
-//Message imgMsg = messageImageCreate()
-
-//!!!!!!!!!!!!! This is needed in order to run tests
-//!!!!!!!!!!!!!!!!!!!!!!!!!!!!! CONVERSATION ADD MESSAGE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-TEST_RESULT_IS(flag, conversationAddMessage(convo1, textMsg1), CONVERSATION_SUCCESS)
-TEST_RESULT_IS(flag, conversationAddMessage(convo1, textMsg2), CONVERSATION_SUCCESS)
-TEST_RESULT_IS(flag, conversationAddMessage(convo1, textMsg3), CONVERSATION_SUCCESS)
-TEST_RESULT_IS(flag, conversationAddMessage(convo1, textMsg4), CONVERSATION_SUCCESS)
-//!!!!!!!!!!!!!!!!!!!!!!!!!!!!! NUMBER OF USERS !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-TEST_RESULT_IS(flag, conversationNumberOfUsers(NULL), -1)
-TEST_RESULT_IS(flag, conversationNumberOfUsers(convo1), 2)
-// TEST_RESULT_IS(flag, conversationNumberOfUsers(convo2), 2345)
-TEST_RESULT_IS(flag, conversationNumberOfUsers(convo2), 0) //why zero, check func signatrue
-//!!!!!!!!!!!!!!!!!!!!!!!!!!!!! MOST TEXT MESSAGES !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-int outSenderID;
-TEST_RESULT_IS(flag,conversationMostTextMessages(convo2, &outSenderID), CONVERSATION_NO_MESSAGES)//convo fail or something
-//!!!!!!!!!!!!!!!!!!!!!!!!!!!! MUST ADD IMAGE CREATER FUNC !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-//TEST_RESULT_IS(flag, conversationMostImages(convo1, &outSenderID), CONVERSATION_SUCCESS)
-Message messageOutput = NULL;
-TEST_RESULT_IS(flag, conversationLastMessage(convo1, 302365697, &messageOutput), CONVERSATION_SUCCESS)
-bool isIdentical;
-TEST_RESULT_IS(flag, messageCompare(messageOutput, textMsg4, &isIdentical), MESSAGE_SUCCESS)//not sure about this
-TEST_RESULT_IS(flag, conversationDeleteMessage(convo1, 302365697, 1), CONVERSATION_SUCCESS)
-TEST_RESULT_IS(flag, conversationDeleteMessage(convo1, 302365697, 0), CONVERSATION_SUCCESS)
-TEST_RESULT_IS(flag, conversationLength(convo1), 2)// check out what goes here
-messageDestroy(textMsg1);
-messageDestroy(textMsg2);
-messageDestroy(textMsg3);
-messageDestroy(textMsg4);
-//delete here two messageImages
-//delete an additonal mesage Image deltere thing
-conversationDestroy(conv1);
-conversationDestroy(conv2);
-
-//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! CONVO CREATE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! there were braces here
-		Conversation convoToCheck = conversationCreate(10);
-
-		int outPutUserID;
-		TEST_RESULT_IS(flag, conversationMostTextMessages(convoToCheck, &outPutUserID), CONVERSATION_NO_MESSAGES)
-		//no messages added here yet
-		Message messageToCheck1 = messageTextCreate(419, "this is strictly for test purposes");
-		Message messageToCheck2 = messageTextCreate(228, "this is for fun");
-		Message messageToCheck3 = messageTextCreate(330, "LMAO");
-
-		TEST_RESULT_IS(flag, conversationMostTextMessages(convoToCheck, &outPutUserID), CONVERSATION_NO_MESSAGES)
-
-		convoResult = conversationAddMessage(convoToCheck, messageToCheck1);
-		convoResult = conversationAddMessage(convoToCheck, messageToCheck1);
-		convoResult = conversationAddMessage(convoToCheck, messageToCheck2);
-		convoResult = conversationAddMessage(convoToCheck, messageToCheck3);
-		convoResult = conversationAddMessage(convoToCheck, messageToCheck2);
-		convoResult = conversationAddMessage(convoToCheck, messageToCheck1);
-
-		TEST_RESULT_IS(flag, conversationMostTextMessages(convoToCheck, &outPutUserID), CONVERSATION_SUCCESS)
-		
-		convoResult = conversationAddMessage(convoToCheck, messageToCheck1);
-		convoResult = conversationAddMessage(convoToCheck, messageToCheck2);
-
-		TEST_RESULT_IS(flag, conversationMostTextMessages(convoToCheck, &outPutUserID), CONVERSATION_SUCCESS)
-		TEST_RESULT_IS(flag, outPutUserID, 419) //id of most pop user
-
-		convoResult = conversationAddMessage(convoToCheck, messageToCheck2);
-		convoResult = conversationAddMessage(convoToCheck, messageToCheck2);
-		convoResult = conversationAddMessage(convoToCheck, messageToCheck2);
-
-
-		TEST_RESULT_IS(flag, conversationMostTextMessages(convoToCheck, &outPutUserID), CONVERSATION_SUCCESS)
-		TEST_RESULT_IS(flag, outPutUserID, 228)
-		//this should change now
-
-		messageDestroy(messageToCheck1);
-		messageDestroy(messageToCheck2);
-		messageDestroy(messageToCheck3);
-
-		conversationDestroy(convoToCheck);
-	
-
-
-//////////////////////////////////////////////////////////////////////////
-	// conversationMostImages
-
-	TEST_RESULT_IS(flag, conversationMostImages(NULL, &outPutUserID), CONVERSATION_NULL_ARGUMENT)
-
-	{
-		int resultUserID;
-		Conversation conversationToCheck = conversationCreate(10);
-
-		int outPutUserID;
-		TEST_RESULT_IS(flag, conversationMostImages(conversationToCheck, &outPutUserID), CONVERSATION_NO_MESSAGES)
-
-		result = conversationAddMessage(conversationToCheck, txt101);
-		TEST_RESULT_IS(flag, conversationMostImages(conversationToCheck, &outPutUserID), CONVERSATION_NO_MESSAGES)
-
-
-		// GENERATE AN IMAGE MESSAGE HERE
-		// Message imgMsgToCheck = generateImageMessage(10, 20, false, 1, 0);// 
-		// Message imgMsgToCheck = generateImageMessage(10, 20, false, 2, 0);
-
-
-
-		result = conversationAddMessage(conversationToCheck, imgMsgToCheck);
-		result = conversationAddMessage(conversationToCheck, imgMsgToCheck);
-		result = conversationAddMessage(conversationToCheck, imgMsgToCheck);
-		result = conversationAddMessage(conversationToCheck, imgMsgToCheck);
-		result = conversationAddMessage(conversationToCheck, imgMsgToCheck);
-		result = conversationAddMessage(conversationToCheck, imgMsgToCheck);
-
-		TEST_RESULT_IS(flag, conversationMostImages(conversationToCheck, &outPutUserID), CONVERSATION_SUCCESS)
-		TEST_RESULT_IS(flag, outPutUserID, 1)
-		
-		result = conversationAddMessage(conversationToCheck, imgMsgToCheck);
-		result = conversationAddMessage(conversationToCheck, imgMsgToCheck);
-
-		TEST_RESULT_IS(flag, conversationMostImages(conversationToCheck, &outPutUserID), CONVERSATION_SUCCESS)
-		TEST_RESULT_IS(flag, outPutUserID, 2)
-
-		result = conversationAddMessage(conversationToCheck, imgMsgToCheck);
-
-		TEST_RESULT_IS(flag, conversationMostImages(conversationToCheck, &outPutUserID), CONVERSATION_SUCCESS)
-		TEST_RESULT_IS(flag, outPutUserID, 1)
-
-		messageDestroy(imgMsgToCheck);
-		messageDestroy(imgMsgToCheck);
-
-		conversationDestroy(conversationToCheck);
+	//RUN_TEST(conversation_test_Combo)
+	bool flag;
+	flag = conversation_test_Combo();
+	if(flag) {
+		printf("SUCCESS");
+	} else {
+		printf("Fail");
 	}
 
-	//////////////////////////////////////////////////////////////////////////
-	// conversationLastMessage
+	return 0;
 
-	Message msgResult;
-	TEST_RESULT_IS(flag, conversationLastMessage(NULL, 2000, &msgResult), CONVERSATION_NULL_ARGUMENT)
-	TEST_RESULT_IS(flag, conversationLastMessage(convLarge, 1999, &msgResult), CONVERSATION_BAD_ID)
-	TEST_RESULT_IS(flag, conversationLastMessage(convLarge, -1, &msgResult), CONVERSATION_BAD_ID)
-	
-	{
-		Conversation conversationToCheck = conversationCreate(10);
 
-		result = conversationAddMessage(conversationToCheck, img102);
+}
 
-		Message imgMsgToCheck = messageTextCreate(1, "user1");
-		Message imgMsgToCheck1 = messageTextCreate(2, "user2msg1");
-		Message imgMsgToCheck2 = messageTextCreate(2, "user2msg2");
-		Message randomMessage;
 
-		result = conversationAddMessage(conversationToCheck, imgMsgToCheck);
-		result = conversationAddMessage(conversationToCheck, imgMsgToCheck);
-		result = conversationAddMessage(conversationToCheck, imgMsgToCheck1);
-		result = conversationAddMessage(conversationToCheck, imgMsgToCheck2);
-		result = conversationAddMessage(conversationToCheck, imgMsgToCheck1);
-		result = conversationAddMessage(conversationToCheck, imgMsgToCheck);
-		result = conversationAddMessage(conversationToCheck, imgMsgToCheck2);
-		result = conversationAddMessage(conversationToCheck, imgMsgToCheck);
+bool conversation_test_Combo() {
+	bool flag = true;
 
-		{
-			Conversation conversationToCheck2 = ConversationCopy(conversationToCheck);
+	TEST_RESULT_IS(flag, conversationCreate(0), NULL)
+			if(!flag){
+				printf("GARDEN OF EDEN\n");
+				flag = true;
+			}
 
-			result = conversationLastMessage(conversationToCheck2, 2, &randomMessage);
-			TEST_RESULT_IS(flag, result, CONVERSATION_SUCCESS)
 
-			bool isIdentical = false;
-			messageCompare(randomMessage, imgMsgToCheck2, &isIdentical);
-			TEST_RESULT_IS(flag, isIdentical, true)
+	Conversation convo1 = conversationCreate(6);
+	Conversation convo2 = ConversationCopy(convo1);
+	Message textMsg1 = messageTextCreate(302365697, "hahahaha");
+	Message textMsg2 = messageTextCreate(132424, "this is it");
+	Message textMsg3 = messageTextCreate(132424, "");
 
-			messageDestroy(randomMessage);
 
-			conversationDeleteMessage(conversationToCheck2, 2, 3);
-			
-			result = conversationLastMessage(conversationToCheck2, 2, &randomMessage);
-			TEST_RESULT_IS(flag, result, CONVERSATION_SUCCESS)
+	unsigned char** img1 = allocateImage(2, 2);
+	if(img1 == NULL) {
+		return 0;
+	}
+	unsigned char** img2 = allocateImage(2, 2);
+	if(img2 == NULL) {
+		freeOldImage(img1,2);
+	}
 
-			isIdentical = false;
-			messageCompare(randomMessage, imgMsgToCheck1, &isIdentical);
-			TEST_RESULT_IS(flag, isIdentical, true)
-				
-			messageDestroy(randomMessage);
+	for(int i  = 0 ; i < 2 ; i++) {
+		for(int j = 0 ; j < 2; j++) {
+			img1[i][j] = j;
+			img2[i][j] = i;
+		}
+	}
 
-			conversationDestroy(conversationToCheck2);
+	Message imgMsg1 = messageImageCreate(302365697, (const unsigned char* const*)img1,2,2);
+	Message imgMsg2 = messageImageCreate(132424, (const unsigned char* const*)img2,2,2);
+
+	TEST_RESULT_IS(flag, conversationAddMessage(convo1, textMsg1), CONVERSATION_SUCCESS)
+		if(!flag){
+			printf("MATAM\n");
+			flag = true;
+		}
+	messageDestroy(textMsg1);
+
+	int outSenderID;
+		TEST_RESULT_IS(flag,conversationMostTextMessages(convo2, &outSenderID), CONVERSATION_NO_MESSAGES)//convo fail or something
+		if(!flag){
+						printf("START TO FINISH\n");
+						flag = true;
+					}
+
+	TEST_RESULT_IS(flag, conversationAddMessage(convo1, textMsg2), CONVERSATION_SUCCESS)
+		if(!flag){
+			printf("NOW\n");
+			flag = true;
+		}
+	TEST_RESULT_IS(flag, conversationAddMessage(convo1, textMsg3), CONVERSATION_SUCCESS)
+		if(!flag){
+			printf("GO\n");
+			flag = true;
 		}
 
-		result = conversationLastMessage(conversationToCheck, 2, &randomMessage);
-		TEST_RESULT_IS(flag, result, CONVERSATION_SUCCESS)
+	TEST_RESULT_IS(flag, conversationAddMessage(convo1, imgMsg1), CONVERSATION_SUCCESS)
+		if(!flag){
+			printf("FINISH\n");
+			flag = true;
+		}
+	TEST_RESULT_IS(flag, conversationAddMessage(convo1, imgMsg2), CONVERSATION_SUCCESS)
+	if(!flag){
+			printf("HAVE\n");
+			flag = true;
+		}
 
-		conversationDestroy(conversationToCheck);
+	TEST_RESULT_IS(flag, conversationDeleteMessage(convo1, 302365697, 37), CONVERSATION_OUT_OF_BOUNDS)
+			if(!flag){
+				printf("CONVO IS OUT OF BOUNDS SON\n");
+				flag = true;
+			}
 
-		bool isIdentical = false;
-		messageCompare(randomMessage, imgMsgToCheck2, &isIdentical);
-		TEST_RESULT_IS(flag, isIdentical, true)
+	//!!!!!!!!!!!!!!!!!!!!!!!!!!!!! NUMBER OF USERS !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	TEST_RESULT_IS(flag, conversationNumberOfUsers(NULL), -1)
+	if(!flag){
+				printf("A SHORT\n");
+				flag = true;
+			}
+	TEST_RESULT_IS(flag, conversationNumberOfUsers(convo1), 2)
+	if(!flag){
+				printf("NAP AND REST\n");
+				flag = true;
+			}
+	TEST_RESULT_IS(flag, conversationNumberOfUsers(convo2), 0) //why zero, check func signatrue
+	if(!flag){
+				printf("AND PLEASE\n");
+				flag = true;
+			}
 
-		messageDestroy(randomMessage);
+	//!!!!!!!!!!!!!!!!!!!!!!!!!!!!! MOST TEXT MESSAGES !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	//int outSenderID;
+	TEST_RESULT_IS(flag,conversationMostTextMessages(convo2, &outSenderID), CONVERSATION_NO_MESSAGES)//convo fail or something
+	if(!flag){
+					printf("START\n");
+					flag = true;
+				}
+	Message messageOutput = NULL;
+	TEST_RESULT_IS(flag, conversationLastMessage(convo1, 302365697, &messageOutput), CONVERSATION_SUCCESS)
+	if(!flag){
+					printf("AGAIN\n");
+					flag = true;
+				}
+	messageDestroy(messageOutput);
 
-		result = conversationLastMessage(convSmall, 101, &randomMessage);
-		TEST_RESULT_IS(flag, result, CONVERSATION_SUCCESS)
-			
-		isIdentical = false;
-		messageCompare(randomMessage, img101, &isIdentical);
-		TEST_RESULT_IS(flag, isIdentical, true)
+	int outUserID = 4;
+		TEST_RESULT_IS(flag, conversationMostImages(convo1, &outUserID), CONVERSATION_SUCCESS)
+		//printf("%d\n", outUserID);
+		if(!flag){
+								printf("WEEKEND YALLLLLLL\n");
+								flag = true;
+							}
 
-		messageDestroy(imgMsgToCheck);
-		messageDestroy(imgMsgToCheck1);
-		messageDestroy(imgMsgToCheck2);
-		messageDestroy(randomMessage);
-	}
+		TEST_RESULT_IS(flag,conversationMostTextMessages(convo2, &outSenderID), CONVERSATION_NO_MESSAGES)//convo fail or something
+			if(!flag){
+							printf("i failed\n");
+							flag = true;
+						}
 
-return flag;
+		TEST_RESULT_IS(flag, conversationLength(convo1), 5)// check out what goes here
+			if(!flag){
+								printf("WEEKEND 11\n");
+								flag = true;
+							}
+
+	TEST_RESULT_IS(flag, conversationDeleteMessage(convo1, 302365697, 0), CONVERSATION_SUCCESS)
+	if(!flag){
+						printf("HAVE A NICE\n");
+						flag = true;
+					}
+	TEST_RESULT_IS(flag, conversationLength(convo1), 4)// check out what goes here
+	if(!flag){
+						printf("WEEKEND\n");
+						flag = true;
+					}
+	Message imgMsg3 = messageImageCreate(302365697, (const unsigned char* const*)img1,2,2);
+	Message imgMsg4 = messageImageCreate(132424, (const unsigned char* const*)img2,2,2);
+	Message imgMsg5 = messageImageCreate(132424, (const unsigned char* const*)img2,2,2);
+
+	TEST_RESULT_IS(flag, conversationAddMessage(convo1, imgMsg3), CONVERSATION_SUCCESS)
+			if(!flag){
+				printf("EVERYBODY WAS\n");
+				flag = true;
+			}
+
+	TEST_RESULT_IS(flag, conversationAddMessage(convo1, imgMsg4), CONVERSATION_SUCCESS)
+			if(!flag){
+				printf("FINISH\n");
+				flag = true;
+			}
+
+	TEST_RESULT_IS(flag, conversationAddMessage(convo1, imgMsg5), CONVERSATION_FULL)
+				if(!flag){
+					printf("FINISH MEEEEEEE\n");
+					flag = true;
+				}
+	freeOldImage(img1, 2);
+	freeOldImage(img2,2);
+	//messageDestroy(textMsg1);
+	messageDestroy(textMsg2);
+	messageDestroy(textMsg3);
+	messageDestroy(imgMsg1);
+	messageDestroy(imgMsg2);
+	messageDestroy(imgMsg3);
+	messageDestroy(imgMsg4);
+	messageDestroy(imgMsg5);
+	//something could be deleted here if i misss up
+	conversationDestroy(convo1);
+	conversationDestroy(convo2);
+
+	return flag;
+
 }
-
